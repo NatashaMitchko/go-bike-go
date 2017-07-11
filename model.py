72bc84571b55f5d5c58a8459ec0ecd7c0a43bbdd
Natasha <natasha.mitchko@gmail.com>
2017-07-11 21:31:08 +0000

    Google Maps JS API credentials, got test pages running locally

--- model.py
+++ model.py
@@ -1,26 +1,24 @@
 """Data model for go-bike-go.
 
 V1: Two tables: User and Station. 
-    Note: Station primary_key is an ID provided directly from citibike
-    GeoAlchemy 2 doesn’t currently support other dialects than 
-    PostgreSQL/PostGIS. 
-
+Note: Station primary_key is an ID provided directly from citibike
 Class names are singular - table names are plural"""
 
 from flask_sqlalchemy import SQLAlchemy
-from geoalchemy2 import Geography
+from geoalchemy2 import Geography, func
 
 db = SQLAlchemy()
+
 
 class User(db.Model):
     """Basic information about the user including home and work locations."""
 
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, 
-                    nullable=False, 
-                    autoincrement=True, 
-                    primary_key=True)
+    id = db.Column(db.Integer,
+                   nullable=False,
+                   autoincrement=True,
+                   primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(50), nullable=False)
@@ -29,7 +27,8 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User id:{id} username:{username}>'.format(username=self.username,
-                                                            id=self.id)
+                                                           id=self.id)
+
 
 class Station(db.Model):
     """Stores information about all available bike stations. Does not store bike
@@ -37,22 +36,23 @@ class Station(db.Model):
 
     __tablename__ = 'stations'
 
-    id = db.Column(db.Integer, 
-                    nullable=False, 
-                    autoincrement=False, 
-                    primary_key=True)
+    id = db.Column(db.Integer,
+                   nullable=False,
+                   autoincrement=False,
+                   primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     point = db.Column(Geography(geometry_type='POINT', srid=4326))
 
     def __repr__(self):
         return '<Station id:{id}>'.format(id=self.id)
 
-################################################################################
+##########################################################################
 
 # What the data from the API looks like:
 # {"station_id":"79","name":"Franklin St & W Broadway","short_name":"5430.08","lat":40.71911552,"lon":-74.00666661,"region_id":71,"rental_methods":["KEY","CREDITCARD"],"capacity":33,"eightd_has_key_dispenser":false},
 # {"station_id":"82","name":"St James Pl & Pearl St","short_name":"5167.06","lat":40.71117416,"lon":-74.00016545,"region_id":71,"rental_methods":["KEY","CREDITCARD"],"capacity":27,"eightd_has_key_dispenser":false},
 # {"station_id":"127","name":"Barrow St & Hudson St","short_name":"5805.05","lat":40.73172428,"lon":-74.00674436,"region_id":71,"rental_methods":["KEY","CREDITCARD"],"capacity":31,"eightd_has_key_dispenser":false}
+
 
 def example_data():
     """Add some example stations and a user to the db"""
@@ -67,7 +67,8 @@ def example_data():
     db.session.add_all([station1, station2, station3])
     db.session.commit()
 
-################################################################################    
+##########################################################################
+
 
 def init_app():
     """Creating Flask app in order to run SQLAlchemy"""
@@ -91,10 +92,5 @@ def connect_to_db(app, database_URI):
 
 if __name__ == '__main__':
 
-        init_app()
-        db.create_all()
-        example_data()
-
-
-
-
+    init_app()
+    db.create_all()
--- server.py
+++ server.py
@@ -1,45 +1,39 @@
 # query: get me the 5 closest stations to 'home'
 # from internet >>> Session.query(Cls).order_by(Cls.geom.distance_box('POINT(0 0)')).limit(10)
+#
+# home, work = db.session.query(User.home, User.work).filter_by(User.id==user_id)
 # 
-# home, work = db.Session.query(User.home, User.work).filter_by(User.id==user_id)
-# closest_to_home = db.Session.query(Stations).order_by(Stations.geom.distance_box(home)).limit(5)
-
-import os, sys, requests
+# main map view, login/register, touch id for login
 from jinja2 import StrictUndefined
+from flask import Flask, render_template, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
-from flask import Flask, jsonify, render_template, redirect, request, flash, session
-from werkzeug.utils import secure_filename
-from model import User, Station, connect_to_db, db
-from flask.ext.bcrypt import Bcrypt
-from sqlalchemy import exc, desc
+
+from model import connect_to_db, db, User, Station
 
 app = Flask(__name__)
-bcrypt = Bcrypt(app)
-
-#### Setup ###
-
-# Required to use Flask sessions and the debug toolbar
-app.secret_key = "B7DB1448B17FB324B053"
-
-# Raise error for undefined variable in Jinja2
+app.secret_key = "ursusmaritimus"
 app.jinja_env.undefined = StrictUndefined
 
-#### End Setup ###
+#---------------------------------------------------------------------#
+
+@app.route('/')
+def index():
+    """Show homepage."""
+
+    return render_template("test.html")
+
+@app.route('/hi')
+def test():
+	"""Test showing a map"""
+
+	return render_template('map_view.html')
 
 
+#---------------------------------------------------------------------#
 
 if __name__ == "__main__":
-
     app.debug = True
-    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-    connect_to_db(app, 'postgres:///bike')
-    db.create_all()
-
-    # make sure templates, etc. are not cached in debug mode
-    app.jinja_env.auto_reload = app.debug
-
-
-    # Use the DebugToolbar
+    connect_to_db(app, 'postgres:///bike_test')
     DebugToolbarExtension(app)
 
-    app.run(port=5000, host='127.0.0.1')   
+    app.run(host="0.0.0.0")
